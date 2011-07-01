93976105538e51991fc424785f4ac2a22661391a
nitind <nitind>
2011-07-01 22:12:24 +0000

    [306958] [Content Assist] No parameters populated for constructor for code completion

--- tests/org.eclipse.wst.jsdt.core.tests.compiler/src/org/eclipse/wst/jsdt/core/tests/compiler/regression/AbstractRegressionTest.java
+++ tests/org.eclipse.wst.jsdt.core.tests.compiler/src/org/eclipse/wst/jsdt/core/tests/compiler/regression/AbstractRegressionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1065,29 +1065,29 @@ public abstract class AbstractRegressionTest extends AbstractCompilerTest
 	 */
 	protected void runNegativeTest(String[] testFiles, String expectedProblemLog) {
 		runNegativeTest(testFiles, expectedProblemLog, null, true);
-		runNegativeTest(testFiles, expectedProblemLog, null /*
+		/*runNegativeTest(testFiles, expectedProblemLog, null 
 															 * no extra class
 															 * libraries
-															 */, true /*
+															 , true 
 																	 * flush
 																	 * output
 																	 * directory
-																	 */,
-				null /* no custom options */,
-				false /* do not generate output */,
-				false /* do not show category */,
-				false /* do not show warning token */, false /*
+																	 ,
+				null  no custom options ,
+				false  do not generate output ,
+				false  do not show category ,
+				false  do not show warning token , false 
 															 * do not skip javac
 															 * for this peculiar
 															 * test
-															 */, false /*
+															 , false 
 																		 * do
 																		 * not
 																		 * perform
 																		 * statements
 																		 * recovery
-																		 */,
-				null);
+																		 ,
+				null);*/
 	}
 
 	/**
