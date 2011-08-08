6cfae5531949d28d9fb17662987549012b931ce0
cjaun <cjaun>
2011-08-08 20:31:59 +0000

    Backing out [352160] - [content assist] Use index to find global functions

--- tests/org.eclipse.wst.jsdt.core.tests.compiler/src/org/eclipse/wst/jsdt/core/tests/compiler/JSDTCompilerTests.java
+++ tests/org.eclipse.wst.jsdt.core.tests.compiler/src/org/eclipse/wst/jsdt/core/tests/compiler/JSDTCompilerTests.java
@@ -26,7 +26,6 @@ import org.eclipse.wst.jsdt.core.tests.compiler.regression.InferTypesTests;
 import org.eclipse.wst.jsdt.core.tests.compiler.regression.UtilTest;
 import org.eclipse.wst.jsdt.core.tests.compiler.util.ExclusionTests;
 import org.eclipse.wst.jsdt.core.tests.interpret.BasicInterpretTest;
-import org.eclipse.wst.jsdt.core.tests.search.SearchTests;
 
 /**
  * Run all compiler regression tests
@@ -62,16 +61,90 @@ public static Test suite() {
 	// interpret tests
 	standardTests.add(BasicInterpretTest.class);
 	
+	
+	
+	
+//	standardTests.addAll(JavadocTest.allTestClasses);
 
+//	standardTests.add(BasicErrorTests.class);
+
+	//	// add all javadoc tests
+//	for (int i=0, l=JavadocTest.ALL_CLASSES.size(); i<l; i++) {
+//		standardTests.add(JavadocTest.ALL_CLASSES.get(i));
+//	}
+//
 	TestSuite all = new TestSuite("JSDT 'Compiler' Tests");
 	all.addTest(ExclusionTests.suite());
 	
+	
+//	int possibleComplianceLevels = AbstractCompilerTest.getPossibleComplianceLevels();
+//	if ((possibleComplianceLevels & AbstractCompilerTest.F_1_3) != 0) {
+//		ArrayList tests_1_3 = (ArrayList)standardTests.clone();
+//		tests_1_3.add(Compliance_1_3.class);
+//		tests_1_3.add(JavadocTest_1_3.class);
+//		// Reset forgotten subsets tests
+//		TestCase.TESTS_PREFIX = null;
+//		TestCase.TESTS_NAMES = null;
+//		TestCase.TESTS_NUMBERS= null;
+//		TestCase.TESTS_RANGE = null;
+//		TestCase.RUN_ONLY_ID = null;
+//		all.addTest(AbstractCompilerTest.buildComplianceTestSuite(AbstractCompilerTest.COMPLIANCE_1_3, tests_1_3));
+//	}
+//	if ((possibleComplianceLevels & AbstractCompilerTest.F_1_4) != 0) {
+//		ArrayList tests_1_4 = (ArrayList)standardTests.clone();
+//		tests_1_4.add(AssertionTest.class);
+//		tests_1_4.add(Compliance_1_4.class);
+//		tests_1_4.add(ClassFileReaderTest_1_4.class);
+//		tests_1_4.add(JavadocTest_1_4.class);
+//		// Reset forgotten subsets tests
+//		TestCase.TESTS_PREFIX = null;
+//		TestCase.TESTS_NAMES = null;
+//		TestCase.TESTS_NUMBERS= null;
+//		TestCase.TESTS_RANGE = null;
+//		TestCase.RUN_ONLY_ID = null;
+//		all.addTest(AbstractCompilerTest.buildComplianceTestSuite(AbstractCompilerTest.COMPLIANCE_1_4, tests_1_4));
+//	}
+//	if ((possibleComplianceLevels & AbstractCompilerTest.F_1_5) != 0) {
+//		ArrayList tests_1_5 = (ArrayList)standardTests.clone();
+//		tests_1_5.addAll(RunComparableTests.ALL_CLASSES);
+//		tests_1_5.add(AssertionTest.class);
+//		tests_1_5.add(ClassFileReaderTest_1_5.class);
+//		tests_1_5.add(GenericTypeSignatureTest.class);
+//		tests_1_5.add(InternalHexFloatTest.class);
+//		tests_1_5.add(JavadocTest_1_5.class);
+//		tests_1_5.add(BatchCompilerTest.class);
+//		tests_1_5.add(ExternalizeStringLiterals15Test.class);
+//		// Reset forgotten subsets tests
+//		TestCase.TESTS_PREFIX = null;
+//		TestCase.TESTS_NAMES = null;
+//		TestCase.TESTS_NUMBERS= null;
+//		TestCase.TESTS_RANGE = null;
+//		TestCase.RUN_ONLY_ID = null;
+//		all.addTest(AbstractCompilerTest.buildComplianceTestSuite(AbstractCompilerTest.COMPLIANCE_1_5, tests_1_5));
+//	}
+//	if ((possibleComplianceLevels & AbstractCompilerTest.F_1_6) != 0) {
+//		ArrayList tests_1_6 = (ArrayList)standardTests.clone();
+//		tests_1_6.addAll(RunComparableTests.ALL_CLASSES);
+//		tests_1_6.add(AssertionTest.class);
+//		tests_1_6.add(ClassFileReaderTest_1_5.class);
+//		tests_1_6.add(GenericTypeSignatureTest.class);
+//		tests_1_6.add(InternalHexFloatTest.class);
+//		tests_1_6.add(JavadocTest_1_5.class);
+//		tests_1_6.add(BatchCompilerTest.class);
+//		tests_1_6.add(ExternalizeStringLiterals15Test.class);
+//		tests_1_6.add(StackMapAttributeTest.class);
+//		// Reset forgotten subsets tests
+//		TestCase.TESTS_PREFIX = null;
+//		TestCase.TESTS_NAMES = null;
+//		TestCase.TESTS_NUMBERS= null;
+//		TestCase.TESTS_RANGE = null;
+//		TestCase.RUN_ONLY_ID = null;
+//		all.addTest(AbstractCompilerTest.buildComplianceTestSuite(AbstractCompilerTest.COMPLIANCE_1_6, tests_1_6));
+//	}
 	for (Iterator iter = standardTests.iterator(); iter.hasNext();) {
 		Class test = (Class) iter.next();
 		all.addTestSuite(test); 
 	}
-	
-	all.addTest(SearchTests.suite());
 	return all;
 } 
 }
